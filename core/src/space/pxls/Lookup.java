754600bdc77dbaa613aa93a6a6c0df8cbc74f316
Andrew Black <andrewblack2656@gmail.com>
2021-01-26 22:03:33 +0100

    Add missing Lookup.java implementation

--- core/src/space/pxls/Lookup.java
+++ core/src/space/pxls/Lookup.java
@@ -1,4 +1,54 @@
 package space.pxls;
 
 public class Lookup {
+    int id;
+    int x;
+    int y;
+    String origin;
+    Integer pixelCount;
+    Integer pixelCountAlltime;
+    long time;
+    String username;
+    String discordName;
+    String faction;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public Integer getPixelCount() {
+        return pixelCount;
+    }
+
+    public Integer getPixelCountAlltime() {
+        return pixelCountAlltime;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getDiscordName() {
+        return discordName;
+    }
+
+    public String getFaction() {
+        return faction;
+    }
 }
